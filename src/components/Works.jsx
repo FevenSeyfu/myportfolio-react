1243e8b44ae54c4f1949bf189c47c3efe9cdd698
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-31 15:52:59 +0300

    add project layout

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,59 +1,54 @@
-import React from 'react'
-import {data} from '../data/data.js'
+import React from "react";
+import { data } from "../data/data.js";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 const Works = () => {
   const project = data;
   return (
-    <div name="Portfolio" className="w-full  md:pt-[80px] md:h-screen text-gray-300 bg-[#0a192f]">
-    <div className="max-w-[1200px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-      <div className="pb-4">
-        <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-red-600">
-          Work
-        </p>
-        <p className="py-2">// Check out some of my recent work</p>
-      </div>
+    <div
+      name="Portfolio"
+      className="w-full pt-6 md:pt-24 text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1200px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-4 ml-16">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-red-600">
+            Projects
+          </p>
+          <p className="py-2">// Check out some of my recent Projects</p>
+        </div>
 
-      {/* container for projects */}
-      <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-4 ">
-        {/* Gird Item */}
+        {/* container for projects */}
         {project.map((item, index) => (
-          <div
-            key={index}
-            style={{ backgroundImage: `url(${item.image})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-            flex justify-center text-center items-center mx-auto content-div"
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                {item.name}
-              </span>
-              <div className="pt-8 text-center ">
-                <a href={item.github} target="_blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                     bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                {/* eslint-disable-next-line */}
-                <a href={item.live} target="_blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                     bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
+          <div className="flex flex-col md:flex-row gap-4 md:mx-16 items-center justify-center h-[25rem]">
+            <img
+              src={item.image}
+              alt={`${item.name} image`}
+              className="md:w-3/6"
+            />
+            <div className="md:w-3/6 h-full gap-4 flex flex-col">
+              <h2 className="text-4xl text-red-600">{item.name}</h2>
+              <p>{item.description}</p>
+              <ul className="flex flex-row gap-2" id="tags">
+                {item.technologies.map((tech, index) => (
+                  <li className="bg-gray-700 text-red-400 text-lg font-bold py-1 px-2 rounded-lg" key={index}>
+                    {tech}
+                  </li>
+                ))}
+              </ul>
+              <div id="buttons" className="flex flex-row justify-evenly">
+                  <a href={item.live} className="flex flex-row items-center gap-2 text-xl font-bold bg-red-600 p-2 rounded-2xl hover:text-red-600 hover:bg-white">
+                    <FaLink size={30}/> Live
+                  </a>
+                  <a href={item.github} className="flex flex-row items-center gap-2 text-xl font-bold border-2 border-red-600 p-2 rounded-2xl hover:text-red-600 hover:bg-white">
+                    <FaGithub  size={30}/> Code
+                  </a>
               </div>
             </div>
           </div>
         ))}
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
--- src/data/data.js
+++ src/data/data.js
@@ -1,86 +1,60 @@
 import QuillHub from '../assets/projects/Quill-hub.png'
 import ScrumFlow from '../assets/projects/scrum-flow.png'
-import BookStoreCMS from '../assets/projects/BookStoreCMS.png'
 import LandingPage from '../assets/projects/manage-landing-page.png'
-import BookStore from '../assets/projects/book-store.png'
 import RestCountries from '../assets/projects/rest-countries.png'
 export const data=[
     {
         id:1,
-        name:"Scrum flow",
-        image:ScrumFlow,
-        description : "A Project Management tool that helps teams organize and manage their projects. It includes features like task creation, assignment, progress tracking with defined RBAC for Product managers,Scrum managers and dev Team",
-        github:"https://github.com/FevenSeyfu/scrumflow",
-        live:"https://scrumflow.netlify.app/",
-        technologies: {
-            1: 'MERN',
-            2: 'JWT',
-            3: 'TAILWINDCSS'
-        }
-    },
-    {
-        id:2,
-        name:"Quill Hub",
-        image:QuillHub,
-        description:'A MERN Stack bloggin app with user authentication,dynamic bloggin and commenting functionality and blog post searching functionality',
-        github:"https://github.com/FevenSeyfu/Quill-Hub",
-        live:"https://quill-hub.netlify.app/",
-        technologies: {
-            1: 'MERN',
-            2: 'JWT',
-            3: 'TAILWINDCSS'
-        }
-    },
-    {
-        id:3,
-        name:"BookStore",
-        image:BookStore,
-        description:'Book Store A fullstack book app with Crud opertations to manage books information and modal to view details',
-        github:"https://github.com/FevenSeyfu/book-store",
-        live:"https://book-store-feven.netlify.app/",
-        technologies: {
-            1: 'MERN',
-            2: 'Netlify',
-            3: 'Tailwind'
-        }
-    },
-    {
-        id:4,
         name:"Manage Landing Page",
         image:LandingPage,
         description:'Manage landing page is a single-page landing crafted for a Frontend Mentor challenge project. With eye catching illustrations, Horizontal testimonial slider,Newsletter signup form with robust error handling.The design is responsive, adapting to diverse screen sizes, and all interactive elements have hover and focus states. ',
         github:"https://github.com/FevenSeyfu/manage-landing-page",
         live:"https://manage-landing-page-feven.netlify.app/",
-        technologies: {
-            1: 'React',
-            2: 'Tailwind',
-            3: 'Vite'
-        }
+        technologies: [
+            'React',
+            'Tailwind',
+            'Vite'
+        ]
     },
     {
-        id:5,
-        name:"BooksStoreCMS",
-        image:BookStoreCMS,
-        description:'BookStoreCMS is a simple bookstore management tool built with React and Redux.',
-        github:"https://github.com/FevenSeyfu/bookstorecms",
-        live:"https://bookstore-feven.netlify.app/",
-        technologies: {
-            1: 'React',
-            2: 'Redux',
-            3: 'CSS'
-        }
-    },
-    {
-        id:6,
+        id:2,
         name:"Rest-Countries with theme switcher",
         image:RestCountries,
-        description:'Rest Countries is a Frontend project that fetches countries information from an api and displays with additional feature to filter countires by region, search by name and switch themes color between dark and light.',
+        description:'Rest Countries is a Frontend project that fetches countries information from an api and displays with additional feature to filter countries by region, search by name and switch themes color between dark and light.',
         github:"https://github.com/FevenSeyfu/Countries-color-theme-switcher",
         live:"https://rest-countries-theme.netlify.app/",
-        technologies: {
-            1: 'React',
-            2: 'context api',
-            3: 'Tailwind'
-        }
+        technologies: [
+            'React',
+            'Context-API',
+            'Tailwind',
+            'Vite'
+        ]
     },
+    {
+        id:3,
+        name:"Scrum flow",
+        image:ScrumFlow,
+        description : "A Project Management tool that helps teams organize and manage their projects. It includes features like task creation, assignment, progress tracking with defined RBAC for Product managers,Scrum managers and dev Team",
+        github:"https://github.com/FevenSeyfu/scrumflow",
+        live:"https://scrumflow.netlify.app/",
+        technologies: [
+            'MERN',
+            'JWT',
+            'TAILWINDCSS',
+        ]
+    },
+    {
+        id:4,
+        name:"Quill Hub",
+        image:QuillHub,
+        description:'A MERN Stack blogging app with user authentication,dynamic blogging and commenting functionality and blog post searching functionality',
+        github:"https://github.com/FevenSeyfu/Quill-Hub",
+        live:"https://quill-hub.netlify.app/",
+        technologies: [
+            'MERN',
+            'JWT',
+            'TAILWINDCSS',
+        ]
+    },
+
 ]
