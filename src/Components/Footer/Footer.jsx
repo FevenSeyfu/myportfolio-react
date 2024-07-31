892411f6c06b86983f9f3f0dfe06547ccd865381
FevenSeyfu <fevenseyfu00@gmail.com>
2024-08-01 00:31:34 +0300

    add ans style footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,25 +3,19 @@ import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-dark-red text-white py-6">
+    <footer className="bg-primary-dark-red text-white py-4 md:py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Feven Seyfu. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} <a href="">Feven Seyfu</a>. All rights reserved.</p>
         <div className="flex justify-center space-x-4 mt-4">
-          <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-secondary-lighter-red">
+          <a href="https://www.linkedin.com/in/fevenseyfu/ " target="_blank" rel="noopener noreferrer" className="hover:text-primary-dark-blue">
             <FaLinkedin size={24} />
           </a>
-          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-secondary-lighter-red">
+          <a href="https://github.com/FevenSeyfu" target="_blank" rel="noopener noreferrer" className="hover:text-primary-dark-blue">
             <FaGithub size={24} />
           </a>
-          <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-secondary-lighter-red">
+          <a href="https://twitter.com/FevenSeyfu" target="_blank" rel="noopener noreferrer" className="hover:text-primary-dark-blue">
             <FaTwitter size={24} />
           </a>
-        </div>
-        <div className="mt-4">
-          <a href="#home" className="hover:text-secondary-lighter-red mx-2">Home</a>
-          <a href="#about" className="hover:text-secondary-lighter-red mx-2">About</a>
-          <a href="#projects" className="hover:text-secondary-lighter-red mx-2">Projects</a>
-          <a href="#contact" className="hover:text-secondary-lighter-red mx-2">Contact</a>
         </div>
       </div>
     </footer>
