84c5107c41c63b58505d82eccc2b86e5fb3f2d53
FevenSeyfu <fevenseyfu00@gmail.com>
2024-08-01 00:32:30 +0300

    update header mobile nav styling

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -17,55 +17,58 @@ const Navbar = () => {
   };
 
   return (
-      <>
-        <nav>
-          {/* menu */}
-          <ul className="hidden md:flex flex-row gap-8">
-            {["home", "about", "Portfolio", "skills", "contact"].map(
-              (section) => (
-                <li key={section}>
-                  <Link
-                    activeClass="text-primary-dark-red border-b-4 border-primary-dark-red text-lg"
-                    to={section}
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className="text-lg font-bold hover:text-secondary-lighter-red hover:border-b-4 border-secondary-lighter-red pb-7"
-                  >
-                    {section.charAt(0).toUpperCase() + section.slice(1)}
-                  </Link>
-                </li>
-              )
-            )}
-          </ul>
-          {/* Hamburger */}
-          <div
-            onClick={handleclick}
-            className="md:hidden text-2xl z-10 hover:text-primary-dark-red"
-          >
-            {!nav ? <FaBars /> : <FaTimes />}
-          </div>
-        </nav>
-        {/* Mobile menu */}
-        <ul
+    <>
+      <nav className="relative z-20">
+        {/* menu */}
+        <ul className="hidden md:flex flex-row gap-8">
+          {["home", "about", "Portfolio", "skills", "contact"].map(
+            (section) => (
+              <li key={section}>
+                <Link
+                  activeClass="text-primary-dark-red border-b-4 border-primary-dark-red text-lg"
+                  to={section}
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="text-lg font-bold hover:text-secondary-lighter-red hover:border-b-4 border-secondary-lighter-red pb-7"
+                >
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </Link>
+              </li>
+            )
+          )}
+        </ul>
+        {/* Hamburger */}
+        <button
+          onClick={handleclick}
+          className="md:hidden text-2xl z-20  text-white"
+        >
+          {!nav ? <FaBars /> : <FaTimes />}
+        </button>
+      </nav>
+      {/* Mobile menu */}
+      <ul
         className={
           !nav
             ? "hidden"
-            : "absolute top-16 right-0 w-[75%] h-screen bg-primary-dark-blue flex flex-col items-center px-8 md:px-12"
+            : "absolute top-0 right-0 max-w-[45%] h-screen  pt-16 bg-primary-dark-red flex flex-col items-center z-10"
         }
       >
         {["home", "about", "Portfolio", "skills", "contact"].map((section) => (
           <li
             key={section}
-            className="py-2 text-2xl w-full text-right text-white  hover:bg-primary-dark-red "
+            className="py-2 text-2xl w-full text-left text-white  hover:bg-primary-dark-blue px-8 md:px-12"
           >
-            <span onClick={() => scrollTo(section)}>
-              {section.charAt(0).toUpperCase() + section.slice(1)}
-            </span>
+            <Link to={section}
+                  spy={true}
+                  smooth={true}
+                  duration={500}>
+            {section.charAt(0).toUpperCase() + section.slice(1)}
+            </Link>
           </li>
         ))}
       </ul>
-      </>
+    </>
   );
 };
 
