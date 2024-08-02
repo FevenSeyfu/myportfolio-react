e117576edcedf79658fed8ca0c84271ca8472db5
FevenSeyfu <fevenseyfu00@gmail.com>
2024-08-02 14:53:16 +0300

    defer third party code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React,{useEffect} from 'react'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About'
-import Skills from './Components/Skills'
-import Works from './Components/Portfolio/Works'
-import Contact from './Components/Contact/Contact'
+import React, { useEffect } from 'react';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Works from './Components/Portfolio/Works';
+import Contact from './Components/Contact/Contact';
 import ReactGA from "react-ga4";
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   useEffect(() => {
-    ReactGA.initialize('G-2V76M8J23J');
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    const loadGoogleAnalytics = () => {
+      ReactGA.initialize('G-2V76M8J23J');
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    };
+
+    if (document.readyState === 'complete') {
+      loadGoogleAnalytics();
+    } else {
+      window.addEventListener('load', loadGoogleAnalytics);
+      return () => window.removeEventListener('load', loadGoogleAnalytics);
+    }
   }, []);
+
   return (
     <div className="flex flex-col min-h-screen bg-primary-dark-blue">
       <Header />
-      <main className="w-full overflow-x-hidden flex-grow px-8 md:pr-0  md:pl-12 lg:pl-[5%]">
+      <main className="w-full overflow-x-hidden flex-grow px-8 md:pr-0 md:pl-12 lg:pl-[5%]">
         <Hero />
         <About />
         <Works />
@@ -25,7 +35,7 @@ const App = () => {
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
